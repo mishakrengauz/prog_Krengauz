9b4cd3bce57e0455010d1a9abc138d23b9703ca4
mishakrengauz <43673087+mishakrengauz@users.noreply.github.com>
2018-12-19 23:44:26 +0300

    Подкорректировал полет снаряда

--- cannon.py
+++ cannon.py
@@ -1,104 +1,106 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  8 22:22:23 2018
-
-@author: mihailkrengauz
-"""
-
 import math
+from tkinter import *
+from random import *
 
-g = 9.8  # Ускорение свободного падения для снаряда.
+screen_width = 1200
+screen_height = 700
+shell_radius = 10
+dt = 0.3
+g = -9.8
 
-class Cannon:
-    max_velocity = 10
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.shell_num = None  # TODO: оставшееся на данный момент количество снарядов
-        self.direction = math.pi / 4
+def screen(x, y):
+    return x, screen_height - y
 
-    def aim(self, x, y):
-        """
-        Меняет направление direction так, чтобы он из точки
-         (self.x, self.y) указывал в точку (x, y).
-        :param x: координата x, в которую целимся
-        :param y: координата y, в которую целимся
-        :return: None
-        """
-        pass  # TODO
-
-    def fire(self, dt):
-        """
-        Создаёт объект снаряда (если ещё не потрачены все снаряды)
-        летящий в направлении угла direction
-        со скоростью, зависящей от длительности клика мышки
-        :param dt:  длительность клика мышки, мс
-        :return: экземпляр снаряда типа Shell
-        """
-        pass
 
 class Shell:
-    standard_radius = 5
+    def __init__(self, x, y, r, Vx, Vy, canvas, k):
+        self.color = 'gray'
+        self.x, self.y, self.r = x, y, r
+        self.Vx, self.Vy = Vx*5, Vy*5
+        self._canvas = canvas
+        self.circle = canvas.create_oval(screen(x - r, y - r), screen(x + r, (y + r)), fill=self.color)
+        self.damage_radius = 40
+        self.damage = 10
+        self.k = k
 
-    def __init__(self, x, y, Vx, Vy):
-        self.x, self.y = x, y
-        self.Vx, self.Vy = Vx, Vy
-        self.r = standard_raduis
-
-    def go(self, dt):
-        """
-        Сдвигает снаряд исходя из его кинематических характеристик
-        и длины кванта времени dt
-        в новое положение, а также меняет его скорость.
-        :param dt:
-        :return:
-        """
-        ax, ay = 0, g
-        self.x += self.Vx*dt + ax*(dt**2)/2
-        self.y += self.Vy*dt + ay*(dt**2)/2
-        self.Vx += ax*dt
-        self.Vy += ay*dt
-        # TODO: Уничтожать (в статус deleted) снаряд, когда он касается земли.
-
-    def detect_collision(self, other):
-        """
-        Проверяет факт соприкосновения снаряда и объекта other
-        :param other: объект, который должен иметь поля x, y, r
-        :return: логическое значение типа bool
-        """
-        length = ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
-        return length <= self.r + other.r
-
-
-class Target:
-    standard_radius = 5
-
-    def __init__(self, x, y, Vx, Vy):
-        self.x, self.y = x, y
-        self.Vx, self.Vy = Vx, Vy
-        self.r = standard_raduis
-
-    def go(self, dt):
-        """
-        Сдвигает шарик-мишень исходя из его кинематических характеристик
-        и длины кванта времени dt
-        в новое положение, а также меняет его скорость.
-        :param dt:
-        :return:
-        """
-        ax, ay = 0, g
+    def move(self):
+        ax = self.k
+        ay = g
         self.x += self.Vx * dt
         self.y += self.Vy * dt
         self.Vx += ax * dt
         self.Vy += ay * dt
-        # TODO: Шарики-мишени должны отражаться от стенок
 
-    def collide(self, other):
-        """
-        Расчёт абсолютно упругого соударения
-        :param other:
-        :return:
-        """
-        pass  #TODO
+        x1, y1 = screen(self.x - self.r, self.y - self.r)
+        x2, y2 = screen(self.x + self.r, self.y + self.r)
+        self._canvas.coords(self.circle, x1, y1, x2, y2)
+
+    def check_collision(self, x, y):
+       l = ((self.x - x) ** 2 + (self.y - y) ** 2) ** 0.5
+       return l <= self.r
+
+    def destroy(self):
+        self._canvas.delete(self.circle)
+
+
+class Cannon:
+    max_cannon_length = 30
+
+    def __init__(self, x, y, canvas, k):
+        self._canvas = canvas
+        self.x, self.y = x, y
+        # self.power = 10
+        # self.on = 0 дописать
+        self.length_x = 0
+        self.length_y = -30
+        self.r = 20
+        self.cannon = self._canvas.create_line(screen(self.x, self.y,), screen(self.x + self.length_x, self.y + self.length_y),
+                                               width=7, fill='black', tag='cannon')
+        self.carriage = self._canvas.create_oval(screen(self.x - self.r, self.y-self.r), screen(self.x+self.r, self.y+self.r),
+                                                fill="black")
+        self.k = k
+        self.health = 100
+        '''
+        canvas.create_text(100, 100, text="здоровье:")  # нужно изменять здоровье. для этого нужны попадания
+        canvas.create_text(150, 100, text=self.health)
+        canvas.create_text(900, 100, text="здоровье:")
+        canvas.create_text(950, 100, text=self.health)
+        '''
+
+    def target(self, x, y):
+        self.length_x = (x - self.x)
+        self.length_y = (y - self.y)
+        l = (self.length_x ** 2 + self.length_y ** 2) ** 0.5
+        self.length_x = self.max_cannon_length * self.length_x / l
+        self.length_y = self.max_cannon_length * self.length_y / l
+
+        x1, y1 = screen(self.x, self.y)
+        x2, y2 = screen(self.x + self.length_x, self.y + self.length_y)
+        self._canvas.delete('cannon')   # нужно исправить
+        self.cannon = self._canvas.create_line(x1, y1, x2, y2, width=7, fill='black', tag='cannon')
+        self._canvas.coords(self.cannon, x1, y1, x2, y2)
+
+    # еще не дописана
+    def power_up(self):
+        if self.on:
+            if self.power < 100:
+                self.power += 1
+            self._canvas.itemconfig(self.cannon, fill='orange')
+        else:
+            self._canvas.itemconfig(self.cannon, fill='black')
+
+    def shoot(self, x, y):
+        self.target(x, y)
+        Vx = self.length_x
+        Vy = self.length_y
+        return Shell(self.x + self.length_x, self.y + self.length_y, shell_radius, Vx, Vy, self._canvas, self.k)
+
+    def take_damage(self, damage):
+        damage = 25
+        self.health -= damage
+        if self.health <= 0:
+            self._canvas.create_text(100, 100, text="здоровье:")  # нужно изменять здоровье. для этого нужны попадания
+            self._canvas.create_text(150, 100, text=self.health)
+            self._canvas.create_text(900, 100, text="здоровье:")
+            self._canvas.create_text(950, 100, text=self.health)
